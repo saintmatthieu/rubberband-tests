951fdc409de3859637b0b371cf150565aeb5020f
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-04-20 14:23:53 +0200

    get some combinations going

--- saint/main.cpp
+++ saint/main.cpp
@@ -3,14 +3,78 @@
 #include "WavFileWriter.h"
 #include "rubberband/RubberBandStretcher.h"
 
-
 #include <cassert>
 #include <filesystem>
 #include <iostream>
+#include <map>
+#include <optional>
 
 namespace fs = std::filesystem;
 using namespace RubberBand;
 using namespace saint;
+using namespace std::literals::string_literals;
+
+std::optional<std::string> asString(RubberBandStretcher::Option option) {
+  switch (option) {
+  case RubberBandStretcher::OptionProcessRealTime:
+    return "ProcessRealTime";
+  case RubberBandStretcher::OptionStretchPrecise:
+    return "StretchPrecise";
+  case RubberBandStretcher::OptionTransientsMixed:
+    return "TransientsMixed";
+  case RubberBandStretcher::OptionTransientsSmooth:
+    return "TransientsSmooth";
+  case RubberBandStretcher::OptionDetectorPercussive:
+    return "DetectorPercussive";
+  case RubberBandStretcher::OptionDetectorSoft:
+    return "DetectorSoft";
+  case RubberBandStretcher::OptionPhaseIndependent:
+    return "PhaseIndependent";
+  case RubberBandStretcher::OptionThreadingNever:
+    return "ThreadingNever";
+  case RubberBandStretcher::OptionThreadingAlways:
+    return "ThreadingAlways";
+  case RubberBandStretcher::OptionWindowShort:
+    return "WindowShort";
+  case RubberBandStretcher::OptionWindowLong:
+    return "WindowLong";
+  case RubberBandStretcher::OptionSmoothingOn:
+    return "SmoothingOn";
+  case RubberBandStretcher::OptionFormantPreserved:
+    return "FormantPreserved";
+  case RubberBandStretcher::OptionPitchHighQuality:
+    return "PitchHighQuality";
+  case RubberBandStretcher::OptionPitchHighConsistency:
+    return "PitchHighConsistency";
+  case RubberBandStretcher::OptionChannelsTogether:
+    return "ChannelsTogether";
+  case RubberBandStretcher::OptionEngineFiner:
+    return "EngineFiner";
+  default:
+    return std::nullopt;
+  }
+}
+
+RubberBandStretcher::Option
+mergeOptions(const std::vector<RubberBandStretcher::Option> &options) {
+  auto merged = 0;
+  for (const auto option : options) {
+    merged |= option;
+  }
+  return static_cast<RubberBandStretcher::Option>(merged);
+}
+
+std::string
+getFileSuffice(const std::vector<RubberBandStretcher::Option> &options) {
+  std::string fileSuffix;
+  std::for_each(options.begin(), options.end(),
+                [&](RubberBandStretcher::Option option) {
+                  if (const auto nonDefaultString = asString(option)) {
+                    fileSuffix += "-"s + *nonDefaultString;
+                  }
+                });
+  return fileSuffix;
+}
 
 int main(int argc, const char *const *argv) {
   if (argc < 2) {
@@ -19,62 +83,78 @@ int main(int argc, const char *const *argv) {
   }
   const fs::path inputPath{std::string{argv[1]}};
 
-  const fs::path outputPath = inputPath.parent_path().append("output.wav");
-  saint::WavFileReader wavReader{inputPath};
-  const auto numChannels = wavReader.getNumChannels();
-  saint::WavFileWriter wavWriter{outputPath, numChannels,
-                                 wavReader.getSampleRate()};
-  RubberBandStretcher stretcher{static_cast<size_t>(wavReader.getSampleRate()),
-                                static_cast<size_t>(numChannels),
-                                RubberBandStretcher::OptionProcessRealTime |
-                                    RubberBandStretcher::OptionChannelsTogether,
-                                2.0};
+  const std::vector<std::vector<RubberBandStretcher::Option>> optionsToCombine{
+      {RubberBandStretcher::OptionChannelsApart,
+       RubberBandStretcher::OptionEngineFaster},
+      {RubberBandStretcher::OptionChannelsApart,
+       RubberBandStretcher::OptionEngineFiner},
+      {RubberBandStretcher::OptionChannelsTogether,
+       RubberBandStretcher::OptionEngineFaster},
+      {RubberBandStretcher::OptionChannelsTogether,
+       RubberBandStretcher::OptionEngineFiner}};
 
-  auto finalCall = false;
+  for (const auto &combination : optionsToCombine) {
+    const auto fileSuffix = getFileSuffice(combination);
+    const fs::path outputPath =
+        inputPath.parent_path() /
+        inputPath.stem().concat("-out").concat(fileSuffix).concat(".wav");
+    const auto mergedOptions = mergeOptions(combination);
+    WavFileReader wavReader{inputPath};
+    const auto numChannels = wavReader.getNumChannels();
+    WavFileWriter wavWriter{outputPath, numChannels, wavReader.getSampleRate()};
+    RubberBandStretcher stretcher{
+        static_cast<size_t>(wavReader.getSampleRate()),
+        static_cast<size_t>(numChannels),
+        RubberBandStretcher::OptionProcessRealTime |
+            RubberBandStretcher::OptionChannelsTogether,
+        2.0};
 
-  {
-    const auto startPad = stretcher.getPreferredStartPad();
-    AudioContainer container(static_cast<int>(startPad),
-                             static_cast<int>(numChannels));
-    stretcher.process(container.get(), startPad, finalCall);
-  }
+    auto finalCall = false;
 
-  auto remainingLeadDelay = stretcher.getStartDelay();
-  while (!finalCall) {
-    const auto numRequired = stretcher.getSamplesRequired();
-    const auto numAvailable =
-        static_cast<size_t>(wavReader.getNumSamplesPerChannelAvailable());
-    const auto numSamplesToRead = std::min(numAvailable, numRequired);
-    AudioContainer readContainer(numSamplesToRead,
-                                 static_cast<int>(numChannels));
-    finalCall = numRequired >= numAvailable;
-    wavReader.read(readContainer.get(), static_cast<int>(numSamplesToRead));
-    stretcher.process(readContainer.get(), numSamplesToRead, finalCall);
-    if (stretcher.available() == -1) {
-      std::cout << "Really ?.." << std::endl;
-      break;
+    {
+      const auto startPad = stretcher.getPreferredStartPad();
+      AudioContainer container(static_cast<int>(startPad),
+                               static_cast<int>(numChannels));
+      stretcher.process(container.get(), startPad, finalCall);
     }
-    const auto numSamplesToWrite = static_cast<size_t>(stretcher.available());
-    AudioContainer writeContainer(numSamplesToWrite, numChannels);
-    const auto retrieved =
-        stretcher.retrieve(writeContainer.get(), numSamplesToWrite);
-    assert(retrieved == numSamplesToWrite);
-    const auto numSamplesToTrim =
-        std::min(numSamplesToWrite, remainingLeadDelay);
-    remainingLeadDelay -= numSamplesToTrim;
-    if (numSamplesToTrim == numSamplesToWrite) {
-      continue;
+
+    auto remainingLeadDelay = stretcher.getStartDelay();
+    while (!finalCall) {
+      const auto numRequired = stretcher.getSamplesRequired();
+      const auto numAvailable =
+          static_cast<size_t>(wavReader.getNumSamplesPerChannelAvailable());
+      const auto numSamplesToRead = std::min(numAvailable, numRequired);
+      AudioContainer readContainer(numSamplesToRead,
+                                   static_cast<int>(numChannels));
+      finalCall = numRequired >= numAvailable;
+      wavReader.read(readContainer.get(), static_cast<int>(numSamplesToRead));
+      stretcher.process(readContainer.get(), numSamplesToRead, finalCall);
+      if (stretcher.available() == -1) {
+        std::cout << "Really ?.." << std::endl;
+        break;
+      }
+      const auto numSamplesToWrite = static_cast<size_t>(stretcher.available());
+      AudioContainer writeContainer(numSamplesToWrite, numChannels);
+      const auto retrieved =
+          stretcher.retrieve(writeContainer.get(), numSamplesToWrite);
+      assert(retrieved == numSamplesToWrite);
+      const auto numSamplesToTrim =
+          std::min(numSamplesToWrite, remainingLeadDelay);
+      remainingLeadDelay -= numSamplesToTrim;
+      if (numSamplesToTrim == numSamplesToWrite) {
+        continue;
+      }
+      wavWriter.write(writeContainer.getWithOffset(numSamplesToTrim),
+                      numSamplesToWrite - numSamplesToTrim);
     }
-    wavWriter.write(writeContainer.getWithOffset(numSamplesToTrim),
-                    numSamplesToWrite - numSamplesToTrim);
-  }
-  // Squeeze out the final few samples
-  while (stretcher.available() > 0) {
-    const auto numSamplesToWrite = static_cast<size_t>(stretcher.available());
-    AudioContainer writeContainer(stretcher.available(), numChannels);
-    const auto retrieved =
-        stretcher.retrieve(writeContainer.get(), numSamplesToWrite);
-    assert(retrieved == numSamplesToWrite);
-    wavWriter.write(writeContainer.get(), numSamplesToWrite);
+    // Squeeze out the final few samples
+    while (stretcher.available() > 0) {
+      const auto numSamplesToWrite = static_cast<size_t>(stretcher.available());
+      AudioContainer writeContainer(stretcher.available(), numChannels);
+      const auto retrieved =
+          stretcher.retrieve(writeContainer.get(), numSamplesToWrite);
+      assert(retrieved == numSamplesToWrite);
+      wavWriter.write(writeContainer.get(), numSamplesToWrite);
+    }
   }
 }
